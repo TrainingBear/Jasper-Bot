7ba8e3593ae8e570d264aee1136235f518650926
TrainingBear <77871999+TrainingBear@users.noreply.github.com>
2026-04-24 19:54:24 +0700

    shadowjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "org.example"
+group = "com.trbear9"
 version = "1.0.0"
 
 repositories {
@@ -11,5 +12,23 @@ repositories {
 
 dependencies {
     implementation("net.dv8tion:JDA:5.3.0")
-    implementation ("com.googlecode.json-simple:json-simple:1.1.1")
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "Jasper.Main"
+        )
+    }
+}
+
+tasks {
+    shadowJar {
+        manifest {
+            attributes(
+                "Main-Class" to "Jasper.Main"
+            )
+        }
+    }
 }
